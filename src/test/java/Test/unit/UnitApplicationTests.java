5ee54dcb3dd3f47a8236a97b224474fcc3808cfd
1101902 <ryool@sk.com>
2022-10-29 16:03:51 +0900

    Feat : Project Setting

---
+++ src/test/java/Test/unit/UnitApplicationTests.java
@@ -0,0 +1,13 @@
+package Test.unit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
